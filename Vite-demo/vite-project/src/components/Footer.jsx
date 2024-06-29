455e8a2772b7c40bfdbee972172ae6439db4ebb5
Viktor-U <viktor7gg@gmail.com>
2024-06-29 18:33:30 +0300

    demo

---
+++ Vite-demo/vite-project/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+export default function Footer(){
+    return(
+
+        <footer className="footer-copy">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-12">
+                <p>
+                  2018 © Applight. Website Designed by{" "}
+                  <a href="http://w3Template.com" target="_blank" rel="dofollow">
+                    W3 Template
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+    );
+}
